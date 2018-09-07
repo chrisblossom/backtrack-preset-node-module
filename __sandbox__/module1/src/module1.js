5d25b3a581d23bf71c3f57ebc625a878c246629f
Chris Blossom <cbloss@gmail.com>
2018-09-07 10:14:39 -0600

    upgrade to babel 7

--- __sandbox__/module1/src/module1.js
+++ __sandbox__/module1/src/module1.js
@@ -3,3 +3,15 @@
 /**
  * module entry file
  */
+
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+async function hello() {
+    await import('./other.js');
+
+    const one = { one: 1 };
+    const two = { two: 1 };
+
+    return { ...one, ...two };
+}
+
+hello();
