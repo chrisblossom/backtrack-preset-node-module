41a1ed3d17fa06049475a60f869e48870b07a3a3
Chris Blossom <cbloss@gmail.com>
2019-01-11 15:59:14 -0800

    sandbox fixes

--- __sandbox__/typescript-module/src/typescript-module.test.ts
+++ __sandbox__/typescript-module/src/typescript-module.test.ts
@@ -1,17 +1,18 @@
 const path = require('path');
+
 const cwd = process.cwd();
 
 const typescriptModule = () => require('./typescript-module')();
 
 beforeEach(() => {
-})
+    process.chdir(path.resolve(__dirname, 'nested'));
+});
 
 afterEach(() => {
     process.chdir(cwd);
-})
+});
 
 test('works', () => {
-    process.chdir(path.resolve(__dirname, 'nested'));
     const result = typescriptModule();
     expect(result).toEqual(1);
 });
