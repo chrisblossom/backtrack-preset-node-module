97a240a6eea41db135ea865a7dda33264f6544d3
Chris Blossom <cbloss@gmail.com>
2019-01-15 18:16:13 -0800

    remove unneeded eslint override

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -4,17 +4,6 @@ module.exports = {
     presets: ['@backtrack/preset'],
 
     config: {
-        eslint: {
-            overrides: [
-                {
-                    files: ['lib/files/package-entry*.js'],
-                    parserOptions: {
-                        sourceType: 'module',
-                    },
-                },
-            ],
-        },
-
         wallaby: (config) => {
             config.files = ['!./__sandbox__/**', ...config.files];
 
